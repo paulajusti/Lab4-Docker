aceb50826df8c7b11d400b8791f6f41eafa17427
paulajusti <paulinhajusti@hotmail.com>
2015-10-14 13:37:22 +0000

    creating lab5 dir

---
+++ lab5/my_application/server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Index File"
+
+@app.route("/hello")
+def hello():
+    return "Hello World"
+
+@app.route("/user/paul")
+def paul():
+    return "User paul"
+
+@app.route("/post/80")
+def post():
+    return "Post 80"
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
